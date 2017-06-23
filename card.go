11f314b22d4662f9b82255ecdcd54c8dd8dfaf6e
Will Roberts <will.roberts11@gmail.com>
2017-06-23 15:01:48 -0700

    Adds basic card API

---
+++ card.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+)
+
+type CardData struct {
+	Cards map[int]Card `json:"cards"`
+}
+
+type Card struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Faction     string `json:"faction"`
+	SetName     string `json:"setName"`
+	Rarity      string `json:"rarity"`
+	Mana        int    `json:"mana"`
+	Attack      int    `json:"attack"`
+	HP          int    `json:"hp"`
+	Type        string `json:"type"`           // General, Minion, Spell, etc.
+	Race        string `json:"race,omitempty"` // E.g. Arcanyst
+	Description string `json:"description"`
+
+	// Debatable utility.
+	//FactionID         int    `json:"factionId"`
+	//RarityID          int    `json:"rarityId"`
+	//Category          string `json:"category"` // unit, spell, etc.
+	//SearchableContent string `json:"searchableContent"`
+	//IsGeneral         bool   `json:"isGeneral"`
+	//IsHidden          bool   `json:"isHidden"`
+	//Frame             string `json:"frame"`
+	//PList             string `json:"plist"`
+	//Sprite            string `json:"sprite"`
+	//Slug              string `json:"slug"`
+	//FactionSlug       string `json:"factionSlug"`
+}
+
+func (c Card) Bytes() []byte {
+	b, err := json.Marshal(c)
+	if err != nil {
+		return []byte{}
+	}
+	return b
+}
+
+func loadCards() (map[int]Card, error) {
+	b, err := ioutil.ReadFile("assets/cards/v1.86.0.json")
+	if err != nil {
+		return map[int]Card{}, err
+	}
+	buf := bytes.NewBuffer(b)
+
+	decoder := json.NewDecoder(buf)
+	data := CardData{}
+	err = decoder.Decode(&data)
+	if err != nil {
+		return map[int]Card{}, nil
+	}
+
+	return data.Cards, nil
+}
--- main.go
+++ main.go
@@ -1,17 +1,43 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
+var (
+	cards map[int]Card
+)
+
 func main() {
+	var err error
+	cards, err = loadCards()
+	if err != nil {
+		log.Fatal("error: failed to load cards:", err)
+	}
+
 	r := mux.NewRouter()
-	r.HandleFunc("/card/%d", CardHandler)
-	http.Handle("/", r)
+	r.HandleFunc("/card/{id}", CardHandler)
+	if err := http.ListenAndServe(":8000", r); err != nil {
+		log.Fatal("error serving http:", err)
+	}
 }
 
-func CardHandler(r *http.Request, w *http.ResponseWriter) {
-	return
+func CardHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	data := cards[toInt(vars["id"])].Bytes()
+
+	w.Write(data)
+	w.Write([]byte("\n"))
+}
+
+func toInt(s string) int {
+	i, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		return 0
+	}
+	return int(i)
 }
